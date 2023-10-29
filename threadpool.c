d467edf68a89915a23a343a4e515b9c8a8c13d85
Shayan Shamsi <shayans210@gmail.com>
2023-10-29 02:40:25 -0700

    completed implementation

--- threadpool.c
+++ threadpool.c
@@ -23,10 +23,12 @@ typedef struct
 task;
 
 // the work queue
-task worktodo;
+struct task task_queue[QUEUE_SIZE];
+int front = 0, rear = -1, size = 0;
 
 // the worker bee
-pthread_t *thread_array;
+pthread_t thread_array[NUMBER_OF_THREADS];
+
 pthread_mutex_t mutex;
 sem_t sem;
 
@@ -37,24 +39,47 @@ int print_error(char *msg) {
 
 // insert a task into the queue
 // returns 0 if successful or 1 otherwise, 
-int enqueue(task t) 
+int enqueue(task t)
 {
+    pthread_mutex_lock(&mutex);
+    if (!(size == QUEUE_SIZE)) {
+        rear = (rear + 1) % QUEUE_SIZE;
+        task_queue[rear] = t;
+        size++;
+    } else {
+        pthread_mutex_unlock(&mutex);
+        return 1;
+    }
+    pthread_mutex_unlock(&mutex);
     return 0;
 }
 
 // remove a task from the queue
-task dequeue() 
+task dequeue()
 {
+    struct task worktodo;
+    pthread_mutex_lock(&mutex);
+    if (!(size == 0)) {
+        worktodo = task_queue[front];
+        front = (front + 1) % QUEUE_SIZE;
+        size--;
+    } else {
+        print_error("ERROR: Forbidden operation. Thread attempted to dequeue from empty queue!")
+    }
+    pthread_mutex_unlock(&mutex);
     return worktodo;
 }
 
 // the worker thread in the thread pool
 void *worker(void *param)
 {
+    struct task worktodo;
     // execute the task
-    execute(worktodo.function, worktodo.data);
-
-    pthread_exit(0);
+    while(TRUE) {
+        sem_wait(&sem);
+        worktodo = dequeue();
+        execute(worktodo.function, worktodo.data);
+    }
 }
 
 /**
@@ -70,25 +95,26 @@ void execute(void (*somefunction)(void *p), void *p)
  */
 int pool_submit(void (*somefunction)(void *p), void *p)
 {
+    struct task worktodo;
+    int ret;
     worktodo.function = somefunction;
     worktodo.data = p;
+    ret = enqueue(worktodo);
+    if (!ret) {
+        sem_post(&sem);
+    }
 
-    return 0;
+    return ret;
 }
 
 // initialize the thread pool
 void pool_init(void)
 {
-    thread_array = malloc(NUMBER_OF_THREADS * sizeof(pthread_t));
-    if (!thread_array) print_error("ERROR: malloc failed");
-
     pthread_mutex_init(&mutex, NULL);
-    sem_init(&sem, 0, 1);
-
+    sem_init(&sem, 0, 0);
     int ret;
-    long t;
-    for (t = 0; t < NUMBER_OF_THREADS; t++) {
-        //ret = pthread_create(&thread_array[t], NULL, //function, //arguments);
+    for (int t = 0; t < NUMBER_OF_THREADS; t++) {
+        ret = pthread_create(&thread_array[t], NULL, worker, NULL);
         if (ret) print_error("ERROR: pthread_create failed");
     }
 }
@@ -96,5 +122,10 @@ void pool_init(void)
 // shutdown the thread pool
 void pool_shutdown(void)
 {
-    pthread_join(bee,NULL);
+    for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+        pthread_cancel(thread_array[i]);
+        pthread_join(thread_array[i], NULL);
+    }
+    sem_destroy(&sem);
+    pthread_mutex_destroy(&mutex);
 }
