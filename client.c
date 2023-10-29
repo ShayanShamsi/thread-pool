f1d7e1ccdb2c723b88d5f8ef75f2b0b4c7e7bdcf
Shayan Shamsi <shayans210@gmail.com>
2023-10-29 02:56:08 -0700

    add more tasks to client

--- client.c
+++ client.c
@@ -12,6 +12,11 @@ struct data
     int b;
 };
 
+int print_error(char *msg) {
+    fprintf(stderr, "%s\n", msg);
+    exit(2);
+}
+
 void add(void *param)
 {
     struct data *temp;
@@ -20,23 +25,34 @@ void add(void *param)
     printf("I add two values %d and %d result = %d\n",temp->a, temp->b, temp->a + temp->b);
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <n>\n", argv[0]);
+        fprintf(stderr, "where <n> is the number of tasks to execute.\n");
+    }
+    int n = argv[1];
+    if (n <= 0) print_error("ERROR: The number of tasks has to be positive.");
+
     // create some work to do
-    struct data work;
-    work.a = 5;
-    work.b = 10;
+    struct data *work = malloc(n * sizeof(struct data));
 
     // initialize the thread pool
     pool_init();
 
     // submit the work to the queue
-    pool_submit(&add,&work);
+    for (int i = 0; i < n; i++) {
+        work[i].a = i;
+        work[i].b = -i;
+        while(pool_submit(&add,&work) != 0) ;
+    }
 
     // may be helpful 
     sleep(3);
 
     pool_shutdown();
+
+    free(work);
 
     return 0;
 }
